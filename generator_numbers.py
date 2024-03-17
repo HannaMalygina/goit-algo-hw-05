0a8b95604c3da5483ef0f4db1a8c933fdf2ee4c2
HannaMalygina <gannamalygina@gmail.com>
2024-03-17 20:53:10 +0100

    space treatment

--- generator_numbers.py
+++ generator_numbers.py
@@ -2,13 +2,12 @@ from typing import Callable
 import re
 
 def generator_numbers(text: str):
-    for word in text.split():
-        #if word.replace(".", "", 1).isdigit(): yield float(word) #change to re
-        if re.search('\d+\.\d+',word): yield float(word) 
+    for match in re.findall('\s\d+\.\d+\s',text): 
+        yield float(match) 
 
 def sum_profit(text, func: Callable):
     return sum(func(text))
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин:000.01 як основний дохід, доповнений додатковими надходженнями 27.45і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
